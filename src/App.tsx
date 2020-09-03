cad5cbe0725b8a7eee3105361fa836c756edade8
Amit Rajput <amitrajput123.ar@gmail.com>
2020-09-04 00:30:45 +0530

    Adding scss plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Canvas } from 'react-three-fiber';
-import 'bootstrap/scss/bootstrap-reboot.scss';
+import 'bootstrap/dist/css/bootstrap-reboot.min.css';
+import './App.scss';
 
 import PerspectiveCamera from "./r3f/PerspectiveCamera";
 import TriangleVortex from "./r3f/TriangleVortex";
@@ -12,10 +13,12 @@ interface AppProps {}
 function App({}: AppProps) {
   return (
     <>
-      <div style={{zIndex: 1, position: 'absolute', color: '#333', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', fontSize: 25, display: 'flex', alignContent: 'center'}}>
-        Hold your horses <Horse/>
+      <div className={'html_root'}>
+        <div className={'hmh'}>
+          Hold my horses <Horse/>
+        </div>
       </div>
-      <Canvas style={{width: '100%', height: '100%', position: 'absolute', backgroundColor: 'grey'}}>
+      <Canvas className={'canvas'}>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <PerspectiveCamera />
